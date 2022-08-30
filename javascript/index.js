d00d5be50f220c3d84e8e6213006a44e79b9aa78
markskwid <bascal.markponce.f.1015@gmail.com>
2022-08-30 19:14:18 +0800

    Second Commit

--- javascript/index.js
+++ javascript/index.js
@@ -12,18 +12,29 @@ $(document).scroll(function () {
 });
 
 const formSubmit = () => {
-  const phone = "";
-  const message = "";
-
   $("#submit-btn").click(() => {
-    let regex_num = /([0-9]|[/\W])/g;
-    const name = $("#fullname").val();
-    const email = $("#email").val();
+    let regex_special = /([A-Za-z\s])/g;
+    let regex_num = /([A-Za-z]|[/\W])/g;
 
-    if (name.match(regex_num)) {
-      $("#fullname").addClass("is-invalid");
+    const name = $("#fullname");
+    const email = $("#email");
+    const phone = $("#phone");
+    const message = $("#message");
+
+    if (name.val() == "" || !name.val().match(regex_special)) {
+      name.addClass("is-invalid");
     } else {
-      $("#fullname").removeClass("is-invalid");
+      // phone
+      if (
+        phone.val() == "" ||
+        phone.val().match(regex_num) ||
+        phone.val().length != 11
+      ) {
+        phone.addClass("is-invalid");
+      } else {
+        phone.removeClass("is-invalid");
+      }
+      name.removeClass("is-invalid");
     }
   });
 };
